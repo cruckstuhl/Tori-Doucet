a8a42bd19379a8d2b712ad99485563e134afce5a
Cole B Ruckstuhl <cruckstuhl@Heisenberg.local>
2019-09-06 21:36:05 -0500

    Added solution for second excercise

--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,12 @@
-testNumber = input("How old are you? ")
-print("You have stated that you are ", testNumber)
+# Programming Excercise 1.7
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(name, " is ", age, " years old.")
+
+
+# Programming Excercise 1.6
+radius = int(input("Enter the radius: "))
+area = (radius * radius) * 3.14
+
+print("The area is ", area, " square units")
